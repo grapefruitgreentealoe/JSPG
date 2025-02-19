b57e00d9709dee311d68831759d4eaed50875e29
grapefruitgreentealoe <76976331+grapefruitgreentealoe@users.noreply.github.com>
2025-02-19 17:50:28 +0900

    answer; BJ_6068

--- greedy/BJ_6068.js
+++ greedy/BJ_6068.js
@@ -1,54 +1,34 @@
-//시간관리하기
-const fs = require("fs");
-const path = require("path");
+const input = require("fs")
+  .readFileSync("input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+const N = +input[0];
 
-// 파일 입력 처리
-const filePath =
-  process.platform === "linux"
-    ? "/dev/stdin"
-    : path.resolve(__dirname, "input.txt");
-const input = fs.readFileSync(filePath, "utf8").trim().split("\n");
-const n = input.shift()[0];
-const arr = input.map((line) => line.split(" ")).map((x) => x.map(Number));
+function sol(works) {
+  let answer = Infinity;
 
-function solution(n, arr) {
-  //가장 늦게까지 해도 되는 것 부터 정하기
-  let biggestTime = 0;
-  const sortedArr = [...arr]
-    .map((x) => [...x])
-    .sort((a, b) => b[1] - a[1])
-    .map((x) => {
-      if (x[1] > biggestTime) biggestTime = x[1];
-      return x;
-    });
-
-  console.log(sortedArr);
-  //-------------------//
-  let filledTime = Array.from({ length: biggestTime }, () => null);
-
-  //이 구간안에서 가능한 시간 구하기.
-  for (let x = 0; x < n; x++) {
-    // console.log(x + 1, sortedArr[x]);
-    if (sortedArr[x][0] > sortedArr[x][1]) return -1;
-    const lastTime = sortedArr[x][1];
-
-    for (let i = lastTime; i > 0; i--) {
-      if (filledTime.slice(i - sortedArr[x][0], i).every((x) => x === null)) {
-        filledTime = filledTime.fill(sortedArr[x][0], i - sortedArr[x][0], i);
-        break;
-      }
-      if (i == 1) {
-        // 못들어간다고 할때
-        return -1;
-      }
-    }
+  //
+  let t = 0;
+  for (let i = 0; i < works.length; i++) {
+    let [time, deadline] = works[i]; //소요시간, 데드라인
+    t += time; // 시간을 더한다 토탈시간.
+    if (t > deadline) return console.log((answer = -1)); //데드라인보다 t가 크가면 리턴
+    if (answer > deadline - t) answer = deadline - t; //데드라인에서 총 소요시간 빼기. 근데 중간에 채워져있는건 어떻게 알지.
+    //그냥 총 소요시간을 빼도 괜찮은 이유?
+    // 데드라인이 빠른 순부터 진행하기 때문에. 현재 t가 가장 빨리시작할수있는 시작점이 되게 때문이다.
   }
-  console.log(filledTime);
-  for (let a = 0; a < biggestTime; a++) {
-    if (filledTime[a]) {
-      return a;
-    }
-  }
+  return console.log(answer);
 }
-
-console.log(solution(n, arr));
+function main() {
+  let works = input.slice(1).map((_) => _.split(" ").map(Number)); // 필요시간, 마감시간.
+  works.sort((a, b) => {
+    let A = a[1];
+    let B = b[1];
+    if (A < B) return -1;
+    else if (A > B) return 1; //오름차순 정리
+    return 0;
+  });
+  sol(works); //오름차순정리된것에 대해서.
+}
+main();
